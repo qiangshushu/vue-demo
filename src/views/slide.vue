b1183819419bee0681778306796ad7243344c2d6
qiangshushu <597515427@qq.com>
2018-08-31 10:35:17 +0800

    qss

---
+++ src/views/slide.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="show = true">打开</button>
+    <div class="slider" :class="{active: show}">
+      asdf
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false
+      };
+    }
+  };
+</script>
+
+
+<style lang="less" scoped>
+  .slider {
+    background-color: red;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    transform: translateX(100%);
+    transition: all 0.3s;
+    &.active {
+      transform: translateX(0);
+    }
+  }
+</style>
+
